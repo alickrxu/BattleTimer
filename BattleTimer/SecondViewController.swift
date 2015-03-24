2d5a98cf511c35f49cf040f143010d91622eba92
alickrxu <alickrxu@gmail.com>
2015-03-23 22:39:21 -0700

    initial commit

---
+++ BattleTimer/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  BattleTimer
+//
+//  Created by Alick Xu on 3/23/15.
+//  Copyright (c) 2015 Alick Xu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
